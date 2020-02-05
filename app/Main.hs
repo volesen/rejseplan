b87447f4f56c6b0f27a02bb7fce0f5088635299b
Vincent Olesen <vincolesen@gmail.com>
2020-02-05 14:28:35 +0100

    Switched to HTTP

--- app/Main.hs
+++ app/Main.hs
@@ -1,16 +1,10 @@
 module Main where
 
 import Network.Wai
-import Network.Wai.Handler.Warp
-import Network.Wai.Handler.WarpTLS
-import Network.Wai.Middleware.Cors (simpleCors)
+import Network.Wai.Handler.Warp (run)
 import Server (app)
 
 main :: IO ()
 main = do
-  print "Running API server on port 8081"
-  runTLS tlsOptions warpOptions app'
-  where
-    app' = simpleCors app
-    tlsOptions = tlsSettings "cert.pem" "secret-key.pem"
-    warpOptions = setPort 8081 defaultSettings
+  print "Running relay server on port 8081"
+  run 8081 app
